db34cc43ec69e03d6005d623c638d426a19c671a
Vitaly <vitalyasatka@gmail.com>
2023-08-16 09:22:32 +0500

    feat: add registration function (ECOMM-49)

--- src/services/sdk/customer.ts
+++ src/services/sdk/customer.ts
@@ -1,7 +1,13 @@
-import { ClientResponse, Customer } from '@commercetools/platform-sdk';
-import { getCustomerApiRoot, projectKey } from './client';
+import {
+  AddressDraft,
+  ClientResponse,
+  Customer,
+  CustomerDraft,
+  CustomerSignInResult,
+} from '@commercetools/platform-sdk';
+import { getAppApiRoot, getCustomerApiRoot, projectKey } from './client';
+import { CountryCode } from '../../types/types';
 
-// eslint-disable-next-line import/prefer-default-export
 export const login = async (email: string, password: string): Promise<Customer> => {
   try {
     const response: ClientResponse<Customer> = await getCustomerApiRoot(email, password)
@@ -13,4 +19,48 @@ export const login = async (email: string, password: string): Promise<Customer> 
   } catch {
     throw new Error('Customer account with the given credentials not found.');
   }
+};
+
+export const createAddressDraft = (
+  streetName: string,
+  city: string,
+  postalCode: string,
+  country: CountryCode
+): AddressDraft => {
+  return {
+    streetName,
+    city,
+    postalCode,
+    country,
+  };
+};
+
+export const createCustomerDraft = (
+  email: string,
+  password: string,
+  firstName: string,
+  lastName: string,
+  dateOfBirth: Date,
+  streetName: string,
+  city: string,
+  postalCode: string,
+  country: CountryCode
+): CustomerDraft => {
+  return {
+    email,
+    password,
+    firstName,
+    lastName,
+    dateOfBirth: dateOfBirth.toISOString().slice(0, 10),
+    addresses: [createAddressDraft(streetName, city, postalCode, country)],
+  };
+};
+
+export const register = async (customerDraft: CustomerDraft): Promise<CustomerSignInResult> => {
+  const response: ClientResponse<CustomerSignInResult> = await getAppApiRoot()
+    .withProjectKey({ projectKey })
+    .customers()
+    .post({ body: customerDraft })
+    .execute();
+  return response.body;
 };
