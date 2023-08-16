8bd651c2d55eae902e7f9c2ba75308016c040038
T. Ustiugov <t.ustiugov@gmail.com>
2023-08-16 08:42:44 +0300

    feat: moved the routes to a separate file (ECOMM-41)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,9 @@
 import COLORS from 'constants/colors';
+import ROUTES from 'router/index';
 import { useRoutes } from 'react-router-dom';
 import { createTheme, ThemeProvider, Box } from '@mui/material';
 import Header from 'components/header/Header';
 import Footer from 'components/footer/Footer';
-import Home from 'pages/tempPages/Home';
-import Shop from 'pages/tempPages/Shop';
-import Contacts from 'pages/tempPages/Contacts';
-import AboutUs from 'pages/tempPages/AboutUs';
-import Blog from 'pages/tempPages/Blog';
-import NotFound from 'pages/tempPages/NotFound';
 import styles from './App.module.scss';
 
 const theme = createTheme({
@@ -23,32 +18,7 @@ const theme = createTheme({
 });
 
 function App() {
-  const routes = useRoutes([
-    {
-      path: '/',
-      element: <Home />,
-    },
-    {
-      path: '/shop',
-      element: <Shop />,
-    },
-    {
-      path: '/blog',
-      element: <Blog />,
-    },
-    {
-      path: '/contacts',
-      element: <Contacts />,
-    },
-    {
-      path: '/about-us',
-      element: <AboutUs />,
-    },
-    {
-      path: '*',
-      element: <NotFound />,
-    },
-  ]);
+  const routes = useRoutes(ROUTES);
 
   return (
     <ThemeProvider theme={theme}>
---
+++ src/router/index.tsx
@@ -0,0 +1,35 @@
+import AboutUs from 'pages/tempPages/AboutUs';
+import Blog from 'pages/tempPages/Blog';
+import Contacts from 'pages/tempPages/Contacts';
+import Home from 'pages/tempPages/Home';
+import NotFound from 'pages/tempPages/NotFound';
+import Shop from 'pages/tempPages/Shop';
+
+const ROUTES = [
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/shop',
+    element: <Shop />,
+  },
+  {
+    path: '/blog',
+    element: <Blog />,
+  },
+  {
+    path: '/contacts',
+    element: <Contacts />,
+  },
+  {
+    path: '/about-us',
+    element: <AboutUs />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+];
+
+export default ROUTES;
