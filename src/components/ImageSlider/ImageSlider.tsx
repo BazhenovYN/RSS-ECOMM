39999f400504b6077e6d9a506a141aa4ae73ef13
BazhenovYN <BazhenovYN@gmail.com>
2023-08-25 13:19:43 +0200

    refactor: migration to mui components (ECOMM-82)

--- src/components/ImageSlider/ImageSlider.tsx
+++ src/components/ImageSlider/ImageSlider.tsx
@@ -1,40 +1,57 @@
 import { useState } from 'react';
-import classNames from 'classnames/bind';
+import { Box, IconButton } from '@mui/material';
+import ArrowBackIosOutlinedIcon from '@mui/icons-material/ArrowBackIosOutlined';
+import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
 import type { Image } from 'types/types';
-
-import styles from './ImageSlider.module.scss';
 
 interface IProps {
   slides: Image[];
+  isCover?: boolean;
 }
 
-function ImageSlider({ slides }: IProps) {
+function ImageSlider({ slides, isCover = false }: IProps) {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const cn = classNames.bind(styles);
-
-  const goToPrevious = () => {
+  const goToPrevious = (event: React.MouseEvent) => {
+    event.stopPropagation();
     const isFirstSlide = currentIndex === 0;
     const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
   };
 
-  const goToNext = () => {
+  const goToNext = (event: React.MouseEvent) => {
+    event.stopPropagation();
     const isLastSlide = currentIndex === slides.length - 1;
     const newIndex = isLastSlide ? 0 : currentIndex + 1;
     setCurrentIndex(newIndex);
   };
 
   return (
-    <div className={cn('slider')}>
-      <button type="button" className={cn('arrow', 'left-arrow')} onClick={goToPrevious}>
-        &#10094;
-      </button>
-      <button type="button" className={cn('arrow', 'right-arrow')} onClick={goToNext}>
-        &#10095;
-      </button>
-      <div className={cn('slide')} style={{ backgroundImage: `url(${slides[currentIndex]?.url})` }} />
-    </div>
+    <Box sx={{ position: 'relative', height: '100%' }}>
+      <IconButton
+        size="large"
+        onClick={goToPrevious}
+        sx={{ position: 'absolute', top: '50%', left: '16px', transform: 'translate(0, -50%)', color: 'white' }}>
+        <ArrowBackIosOutlinedIcon fontSize="large" sx={{ color: 'white' }} />
+      </IconButton>
+      <IconButton
+        size="large"
+        onClick={goToNext}
+        sx={{ position: 'absolute', top: '50%', right: '16px', transform: 'translate(0, -50%)', color: 'white' }}>
+        <ArrowForwardIosOutlinedIcon fontSize="large" sx={{ color: 'white' }} />
+      </IconButton>
+      <Box
+        sx={{
+          width: '100%',
+          height: '100%',
+          backgroundColor: 'black',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+          backgroundImage: `url(${slides[currentIndex]?.url})`,
+          backgroundSize: isCover ? 'cover' : 'contain',
+        }}
+      />
+    </Box>
   );
 }
 
