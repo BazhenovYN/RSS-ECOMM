366b14a37f310d89464562f25d980b889058bb8a
Vitaly <vitalyasatka@gmail.com>
2023-08-27 11:55:15 +0500

    refactor: add interactivity (ECOMM-78)

--- src/components/CatalogProductItem/CatalogProductItem.tsx
+++ src/components/CatalogProductItem/CatalogProductItem.tsx
@@ -2,7 +2,7 @@ import { DEFAULT_LANGUAGE } from 'constants/const';
 import logo from 'assets/img/logo.png';
 import { useContext } from 'react';
 import AppContext from 'context';
-import { Box, Card, CardContent, CardHeader, CardMedia, Typography, Stack } from '@mui/material';
+import { Box, Card, CardContent, CardHeader, CardMedia, Typography, Stack, useTheme } from '@mui/material';
 import { Product } from 'types/types';
 
 interface CatalogProductItemProps {
@@ -13,6 +13,8 @@ function CatalogProductItem({ product }: CatalogProductItemProps) {
   const appContext = useContext(AppContext);
   const language = appContext?.language;
 
+  const theme = useTheme();
+
   const imgUrl = product.images?.[0]?.url;
   const name = (language && product.name[language]) || product.name[DEFAULT_LANGUAGE];
   const description = language && product.description?.[language];
@@ -20,7 +22,18 @@ function CatalogProductItem({ product }: CatalogProductItemProps) {
   const discountedPrice = product?.discountedCost ? `${product.currency} ${product.discountedCost}` : null;
 
   return (
-    <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+    <Card
+      sx={{
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        transition: 'box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms, scale 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+        '&:hover': {
+          boxShadow: `0 2px 5px 1px ${theme.palette.primary.main}`,
+          cursor: 'pointer',
+          scale: '1.05',
+        },
+      }}>
       {imgUrl ? (
         <CardMedia component="img" height="200" sx={{ minHeight: '200px' }} image={imgUrl} alt={name} />
       ) : (
