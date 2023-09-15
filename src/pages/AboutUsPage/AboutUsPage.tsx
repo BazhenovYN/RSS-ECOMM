c3ffdd6d0f3c221be84d5a36268027d9ee3c5c80
Vitaly <vitalyasatka@gmail.com>
2023-09-15 11:13:55 +0500

    feat: add contributions and collaboration (ECOMM-116)

--- src/pages/AboutUsPage/AboutUsPage.tsx
+++ src/pages/AboutUsPage/AboutUsPage.tsx
@@ -1,9 +1,25 @@
-import { Box, Card, CardActions, CardContent, CardHeader, CardMedia, Link, Stack, Typography } from '@mui/material';
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Link,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Stack,
+  Typography,
+} from '@mui/material';
 import photoBazhenov from 'assets/img/Iurii_Bazhenov.jpg';
 import photoFatkullin from 'assets/img/Vitaly_Fatkullin.jpg';
 import styles from 'pages/AboutUsPage/AboutUsPage.module.scss';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import { Diversity3, QuestionAnswer, Sms, Star, Task } from '@mui/icons-material';
 
+// eslint-disable-next-line max-lines-per-function
 function AboutUsPage() {
   return (
     <Box>
@@ -54,10 +70,23 @@ function AboutUsPage() {
               Contribution:
             </Typography>
             <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi cursus a turpis vitae scelerisque. Interdum
-              et malesuada fames ac ante ipsum primis in faucibus. Nulla facilisi. Cras varius justo eget semper
-              lacinia. Aenean lobortis, tellus sit amet rutrum rutrum, elit lorem pharetra ex, sed vestibulum velit
-              dolor a ante. Cras tempus metus eget mollis convallis.
+              <List>
+                <ListItem>
+                  <ListItemText primary="Participated in analyzing and selecting technical solutions, tools and libraries used in the project" />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="Conducted prioritization and distribution of tasks" />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="Ensured communication and collaboration within the team" />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="Implemented a number of key mechanisms such as a personal user account, shopping cart and many others" />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="Performed testing of new functionality, user interface and overall performance of the application" />
+                </ListItem>
+              </List>
             </Typography>
           </CardContent>
           <CardActions sx={{ marginTop: 'auto' }}>
@@ -104,12 +133,20 @@ function AboutUsPage() {
             <Typography component="h6" variant="h6">
               Contribution:
             </Typography>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi cursus a turpis vitae scelerisque. Interdum
-              et malesuada fames ac ante ipsum primis in faucibus. Nulla facilisi. Cras varius justo eget semper
-              lacinia. Aenean lobortis, tellus sit amet rutrum rutrum, elit lorem pharetra ex, sed vestibulum velit
-              dolor a ante. Cras tempus metus eget mollis convallis.
-            </Typography>
+            <List>
+              <ListItem>
+                <ListItemText primary="Actively participated in the discussion of requirements for the application functionality" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Implemented a number of key mechanisms such as API integration, authentication, product catalog and many others" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="During code-reviews offered recommendations to improve the quality and efficiency of the code base" />
+              </ListItem>
+              <ListItem>
+                <ListItemText primary="Performed testing of new functionality, user interface and overall performance of the application" />
+              </ListItem>
+            </List>
           </CardContent>
           <CardActions sx={{ marginTop: 'auto' }}>
             <Link href="https://github.com/ImmuneQQ" target="_blank">
@@ -118,17 +155,46 @@ function AboutUsPage() {
           </CardActions>
         </Card>
       </Stack>
-      <Typography component="h3" variant="h3" textAlign="center" mb={3}>
-        Collaboration
-      </Typography>
-      <Typography textAlign="center">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi cursus a turpis vitae scelerisque. Interdum et
-        malesuada fames ac ante ipsum primis in faucibus. Nulla facilisi. Cras varius justo eget semper lacinia. Aenean
-        lobortis, tellus sit amet rutrum rutrum, elit lorem pharetra ex, sed vestibulum velit dolor a ante. Cras tempus
-        metus eget mollis convallis. Nunc ut lorem aliquam, consequat nunc vel, ullamcorper arcu. Proin scelerisque
-        sapien iaculis consectetur lacinia. Phasellus arcu lectus, aliquet quis luctus vel, consequat consequat augue.
-        Curabitur porttitor felis diam, ut mollis ligula lacinia lacinia. Quisque laoreet et enim quis pellentesque.
-      </Typography>
+      <Stack maxWidth="1084px" mr="auto" ml="auto" mb={3}>
+        <Typography component="h3" variant="h3" textAlign="center" mb={3}>
+          Collaboration
+        </Typography>
+        <Typography component="h5" variant="h5" textAlign="center" mb={2}>
+          To achieve a successful outcome, we adhered to the following principles:
+        </Typography>
+        <List className={styles.list}>
+          <ListItem className={styles['list-item']}>
+            <ListItemIcon>
+              <Sms color="primary" />
+            </ListItemIcon>
+            <ListItemText primary="Clear and regular communication" />
+          </ListItem>
+          <ListItem className={styles['list-item']}>
+            <ListItemIcon>
+              <Task color="primary" />
+            </ListItemIcon>
+            <ListItemText primary="Effective division of tasks and responsibilities" />
+          </ListItem>
+          <ListItem className={styles['list-item']}>
+            <ListItemIcon>
+              <QuestionAnswer color="primary" />
+            </ListItemIcon>
+            <ListItemText primary="Constructive feedback and error correction" />
+          </ListItem>
+          <ListItem className={styles['list-item']}>
+            <ListItemIcon>
+              <Diversity3 color="primary" />
+            </ListItemIcon>
+            <ListItemText primary="Mutual assistance, support and cooperation" />
+          </ListItem>
+          <ListItem className={styles['list-item']}>
+            <ListItemIcon>
+              <Star color="primary" />
+            </ListItemIcon>
+            <ListItemText primary="Striving of each team member to create the best possible product" />
+          </ListItem>
+        </List>
+      </Stack>
     </Box>
   );
 }
